b6c36c90a8f4a1373f6f2b2c98ce31408f976b4a
Steve Witman <stevewitman@gmail.com>
2017-02-12 16:22:03 -0700

    Add nav header and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { AppComponent } from './app.component';
 import { MarketComponent } from './market/market.component';
 import { CollectionComponent } from './collection/collection.component';
 import { CollectableService } from './shared/collectable.service';
+import { HeaderComponent } from './header.component';
+import { routing } from './app.routing'
 
 @NgModule({
   declarations: [
     AppComponent,
     MarketComponent,
     MarketComponent,
-    CollectionComponent
+    CollectionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [CollectableService],
   bootstrap: [AppComponent]
